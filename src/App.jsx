ca49119a13eb828418a64bc851e24d5c9ac8dba3
prakharp18 <pporwal2019@gmail.com>
2025-07-17 09:36:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="text-3xl font-heading underline">
+    space grotesk
+  </h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// ⛳️ Fix for __dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
+})
